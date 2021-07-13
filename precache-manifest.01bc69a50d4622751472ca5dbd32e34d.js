764d9e88a914060907738bdb96977feadfac1e3e
Sanket-Sinha-03 <sinhaawi04@gmail.com>
2021-07-13 22:47:35 +0530

    Updates

---
+++ precache-manifest.01bc69a50d4622751472ca5dbd32e34d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1704bb7584cc2d099572312dbc37c779",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "69b50061ad7316a79626",
+    "url": "/Covid-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "bfa3b69fafbddce613b1",
+    "url": "/Covid-19-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "69b50061ad7316a79626",
+    "url": "/Covid-19-Tracker/static/js/2.a1eacb32.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Tracker/static/js/2.a1eacb32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa3b69fafbddce613b1",
+    "url": "/Covid-19-Tracker/static/js/main.d548fedf.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  }
+]);
